eed530ba47297bb6a3fedd89bb5f215af99bb2af
Grimka <julien.decroix64@gmail.com>
2019-02-06 19:44:51 +0100

    adding gui support

--- src/GameScreenManager.js
+++ src/GameScreenManager.js
@@ -84,6 +84,9 @@ GameScreensManager.prototype.add = function( screen )
   this.screens[ screen.name ] = screen;
   this.screens[ screen.name ].on( "changeScreen", this.changeScreen, this );
   this.render.add( screen.camera );
+  if( screen.gui )
+    this.render.add( screen.gui );
+    
   screen.hide( undefined, undefined, true );
 };
 
